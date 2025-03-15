c2f42a03b587b90e470269ba7fa559d1537d29b6
Glitch (minipotato-bot-by-eoi) <none>
2025-03-15 08:25:36 +0000

    🐏🍨 Checkpoint ./commands/taisen/omikuji.mjs:76274936/495

--- commands/taisen/omikuji.mjs
+++ commands/taisen/omikuji.mjs
@@ -5,8 +5,8 @@ export const data = new SlashCommandBuilder()
   .setDescription("おみくじを引きます");
 
 export async function execute(interaction) {
-  const arr = ["魔王", "軍神", "百合神", "乙牌", "撃破王", "大吉", "吉", "中吉", "小吉", "もみ吉", "抹吉", "凶", "大凶", "大戦犯"];
-  const weight = [7.91, 2.09, 2, 3, 3, 4, 5, 10, 10, 10, 15, ];
+  const arr = ["魔　王", "軍　神", "百　合　神", "乙　牌", "撃　破　王", "大　吉", "吉", "中　吉", "小　吉", "も　み　吉", "抹　吉", "凶", "大　凶", "大　戦　犯"];
+  const weight = [7.91, 2.09, 2, 3, 3, 4, 7, 8, 12, 12, 15, 8, 8, 8 ]; // 100%換算　魔王が7.91%で出やすくなるように
   let result = "";
 
   let totalWeight = 0;
@@ -26,6 +26,13 @@ export async function execute(interaction) {
 
   // 絵文字を追加する（カスタム絵文字IDは Discord中で\:emoji:と打ち込めば返る
   // 1322580002491072653 = mao791
-  const emoji = "<:custom_emoji:1322580002491072653>";
-  await interaction.reply(`${result} が当選しました！`);
+  // 1322580258125516953 = matcha
+  // 1322730562145882122 = ee
+  // 1329812204475777066 = yattane
+  // 1322843082399547483 = betoman_viet
+  // 1322844796091367477 = gekido
+  // 1350383778643841065 = yurigami
+  
+  const mao = "<:custom_emoji:1322580002491072653>";
+  await interaction.reply(`運　勢　：　${result} ！`);
 }
