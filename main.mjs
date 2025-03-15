3c042568a0362f747e8d4bfa6f8b37f6c5855a61
Glitch (minipotato-bot-by-eoi) <none>
2025-03-15 06:16:37 +0000

    🍮💃 Checkpoint ./main.mjs:76274936/128

--- main.mjs
+++ main.mjs
@@ -86,7 +86,7 @@ client.on("messageCreate", async (message) => {
 });
 
 client.on("ready", async () => {
-  await client.user.setActivity('🥔', { type: ActivityType.Custom, state: "🥔を栽培中" });
+  await client.user.setActivity('大戦');
   console.log(`${client.user.tag} がログインしました！`);
 });
 
