b13c093c85db090b46628a8b441f2bc3420696e9
Glitch (minipotato-bot-by-eoi) <none>
2025-03-23 08:51:43 +0000

    🍮📅 Checkpoint ./commands/kaikyu/kaikyu.mjs:76274936/365

--- commands/kaikyu/kaikyu.mjs
+++ commands/kaikyu/kaikyu.mjs
@@ -3,6 +3,10 @@ import { GameState, User } from '../taisen/game.js';
 
 const ranks = ['二等兵', '一等兵', '軍曹', '曹長', '大尉', '大佐', '准将', '大将', '元帥'];
 
+// 軍名設定　変更はここから
+const nameA = 'きのこ軍';
+const nameB = 'たけのこ軍';
+
 export const data = new SlashCommandBuilder()
   .setName('kaikyu')
   .setDescription('軍を選択し、ランダムな階級を割り当てます')
@@ -11,22 +15,23 @@ export const data = new SlashCommandBuilder()
       .setDescription('所属する軍を選択')
       .setRequired(true)
       .addChoices(
-        { name: 'きのこ軍', value: 'A' },
-        { name: 'たけのこ軍', value: 'B' }
+        { name: `${nameA}`, value: 'A' },
+        { name: `${nameB}`, value: 'B' }
       ));
 
-  // gekiha.mjsで表示するためのグローバル関数の設定　軍命の変更をkaikyu.
-export const armyName_global = {A:'きのこ軍',B:'たけのこ軍'};
+  // gekiha.mjsで表示するためのグローバル関数の設定　軍命の変更をkaikyu.mjsだけで留める
+export const armyName_global = {A:`${nameA}`,B:`${nameB}`};
 export function getArmyName(army) {
   return armyName_global[army] || '不明';
 }
+
 export async function execute(interaction) {
   const army = interaction.options.getString('army');
   const userId = interaction.user.id;
   const username = interaction.user.username;
 
     // army の値に対応する軍名を取得
-  const armyName = army === 'A' ? 'きのこ軍' : 'たけのこ軍';
+  const armyName = army === 'A' ? `${nameA}` : `${nameB}`;
   
   try {
     // ルールが設定されているか確認
@@ -38,7 +43,7 @@ export async function execute(interaction) {
     // すでに登録済みか確認
     const existingPlayer = await User.findOne({ where: { id: userId } });
     if (existingPlayer) {
-      const existingArmyName = existingPlayer.army === 'A' ? 'きのこ軍' : 'たけのこ軍';
+      const existingArmyName = existingPlayer.army === 'A' ? `${nameA}` : `${nameB}`;
       return await interaction.reply(`エラー: あなたはすでに **${existingArmyName}** の **${existingPlayer.rank}** です！`);
     }
 
