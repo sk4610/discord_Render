57b4ad891f052fd05291391203734e51d4992177
Glitch (minipotato-bot-by-eoi) <none>
2025-05-03 00:52:09 +0000

    🏣📉 Checkpoint ./commands/taisen/gekiha.mjs:76274936/160

--- commands/taisen/gekiha.mjs
+++ commands/taisen/gekiha.mjs
@@ -56,11 +56,7 @@ export async function execute(interaction) {
       await interaction.reply('エラー: 未知のルール「${rule_type}」です。');
     } 
     
-    if (gameState.bobEnabled) {
-      const bobId = `bob-${userId}`;
-      const bobUser = await User.findOne({ where: { id: bobId } });
-      if (bobUser) {
-        
+      
         
     // 終戦判定
     // initialArmyHPはカウントダウン方式しか使わないためカウントダウンしか判定されない
